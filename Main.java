4ebceea941a5caac641d686633877fd35fa10a15
PhaySometh <phay.someth70@gmail.com>
2025-01-23 13:44:28 +0700

    fiexd some code in main

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 import Class.Nut;
+import Class.Supplier;
+import Class.Customer;
+import Class.Order;
+import Class.OrderItem;
+import Class.Inventory;
 
 public class Main {
     public static void main(String[] args) {
         
-        Nut myNut = new Nut();
+        Nut myNut = new Nut("Cashew", "drupe", 5, "KompongCham", 10 );
 
-
+        System.out.println("This is Nut information: "
+                            + "\nName: " + myNut.getName()
+                            + "\nType: " + myNut.getType()
+                            + "\nPrice per Kg:" + myNut.getPricePerKg()
+                            + "\nOrigin:" + myNut.getOrigin()
+                            + "\nstockQuantity:" + myNut.getStockQuantity()
+                        );
     }
 }
